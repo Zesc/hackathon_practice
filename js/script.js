0459864770789ed731fefad4d79eb6fdc233dc60
Zesc <jeddak.cesc@gmail.com>
2016-10-22 22:11:39 -0700

    header section

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
-// window.onload = function () {
-//   (function () {
-//
-//     var keyEvent = {
-//       init: function () {
-//         this.cacheDOM();
-//         this.bindEvents();
-//       },
-//       cacheDOM: function () {
-//         this.form = document.getElementById("proj_form");
-//         this.field = document.getElementById("name");
-//       },
-//       bindEvents: function () {
-//         for (var i = 0; i < this.form.children.length; i++) {
-//           if (this.form.children[i].nodeName === "INPUT") {
-//             console.log('a');
-//             this.form.children[i].addEventListener("keyup", this.test(i));
-//
-//           }
-//         }
-//
-//           // this.form.addEventListener("keyup",function () {
-//           //   for (var i = 0; i < keyEvent.form.children.length; i++) {
-//           //     if (keyEvent.form.children[i].nodeName === "INPUT" && event.key === "Enter") {
-//           //       this.children[i].setAttribute("autofocus","autofocus");
-//           //     }
-//           //   }
-//           // });
-//       },
-//       test: function (i) {
-//         console.log(event.key);
-//         if (event.key === "Enter") {
-//           console.log('h');
-//             this.form.children[i].setAttribute("autofocus","autofocus");
-//         }
-//       }
-//     };
-//
-//     keyEvent.init();
-//   }());
-//
-// };
+window.onload = function () {
+  (function () {
+
+    var keyEvent = {
+      init: function () {
+        this.cacheDOM();
+        this.bindEvents();
+      },
+      cacheDOM: function () {
+        this.form = document.getElementById("proj_form");
+        this.field = document.getElementById("name");
+      },
+      bindEvents: function () {
+        for (var i = 0; i < this.form.children.length; i++) {
+          if (this.form.children[i].nodeName === "INPUT") {
+            console.log('a');
+            this.form.children[i].addEventListener("keyup", this.test(i));
+
+          }
+        }
+
+          // this.form.addEventListener("keyup",function () {
+          //   for (var i = 0; i < keyEvent.form.children.length; i++) {
+          //     if (keyEvent.form.children[i].nodeName === "INPUT" && event.key === "Enter") {
+          //       this.children[i].setAttribute("autofocus","autofocus");
+          //     }
+          //   }
+          // });
+      },
+      test: function (i) {
+        console.log(event.key);
+        if (event.key === "Enter") {
+          console.log('h');
+            this.form.children[i].setAttribute("autofocus","autofocus");
+        }
+      }
+    };
+
+    keyEvent.init();
+  }());
+
+};
